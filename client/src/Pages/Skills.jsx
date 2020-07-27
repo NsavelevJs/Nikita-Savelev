024983971b337a97d7e04a4f918655dc2cca46ed
NsavelevJS <NsavelevJS@gmail.com>
2020-07-27 16:46:56 -0400

    Working on the work page

--- client/src/Pages/Skills.jsx
+++ client/src/Pages/Skills.jsx
@@ -3,12 +3,12 @@ import React, { Component } from "react";
 class Skills extends Component {
   render() {
     return (
-      <div class="container mx-auto p-8 ">
+    //   <div class="container mx-auto p-8 ">
 <div class="px-4 py-4 mx-auto">
             <div class="grid sm:h-24 sm:grid-flow-row sm:gap-4 sm:grid-cols-3 grid-cols-1 md:grid-cols-4">
                 <div class="shadow-lg flex flex-col justify-center bg-white border border-gray-300 rounded">
                     <div class="flex w-full h-full">
-                        <div class="w-1/3 flex items-center justify-center bg-yellow-500">
+                        <div class="w-1/3 flex items-center justify-center bg-yellow-300">
                             <p class="text-3xl font-semibold text-center text-black">JS</p>
                         </div>
                         <div class="w-2/3 flex items-center justify-center">
@@ -17,12 +17,23 @@ class Skills extends Component {
                         </div>
                     </div>
                 </div>
+                <div class="shadow-lg flex flex-col justify-center bg-white border border-gray-300 rounded">
+                    <div class="flex w-full h-full">
+                        <div class="w-1/3 flex items-center justify-center bg-yellow-300">
+                            <p class="text-3xl font-semibold text-center text-black">ExJS</p>
+                        </div>
+                        <div class="w-2/3 flex items-center justify-center">
+                   
+                            <p class="text-2xl text-center text-gray-900">Express.js</p>
+                        </div>
+                    </div>
+                </div>
 
                 <div class="shadow-lg flex flex-col justify-center bg-white border border-gray-300 rounded">
                     <div class="flex w-full h-full">
-                        <div class="w-1/3 flex items-center justify-center bg-red-500">
-                        {/* <i class="far fa-gem"></i> */}
-                            <p class="text-3xl font-semibold text-center text-black">rb</p>
+                        <div class="w-1/3 flex items-center justify-center bg-red-700">
+                   
+                            <p class="text-3xl font-semibold text-center text-black">Rb</p>
                         </div>
                         <div class="w-2/3 flex items-center justify-center ">
                             <p class="text-2xl text-center text-gray-900">Ruby</p>
@@ -31,9 +42,9 @@ class Skills extends Component {
                 </div>
  <div class="shadow-lg flex flex-col justify-center bg-white border border-gray-300 rounded">
                     <div class="flex w-full h-full">
-                        <div class="w-1/3 flex items-center justify-center bg-red-500">
-                        {/* <i class="fab fa-github"></i> */}
-                            <p class="text-3xl font-semibold text-center text-black">rbr</p>
+                        <div class="w-1/3 flex items-center justify-center bg-red-700">
+                   
+                            <p class="text-3xl font-semibold text-center text-black">Rbr</p>
                         </div>
                         <div class="w-2/3 flex items-center justify-center ">
                             <p class="text-2xl text-center text-gray-900">Ruby on Rails</p>
@@ -43,7 +54,7 @@ class Skills extends Component {
                 <div class="shadow-lg flex flex-col justify-center bg-white border border-gray-300 rounded">
                     <div class="flex w-full h-full">
                         <div class="w-1/3 flex items-center justify-center bg-blue-500">
-                        {/* <i class="fab fa-react"></i> */}
+                      
                             <p class="text-3xl font-semibold text-center text-black">R</p>
                         </div>
                         <div class="w-2/3 flex items-center justify-center ">
@@ -56,7 +67,7 @@ class Skills extends Component {
                 <div class="shadow-lg flex flex-col justify-center bg-white border border-gray-300 rounded">
                     <div class="flex w-full h-full">
                         <div class="w-1/3 flex items-center justify-center bg-teal-500">
-                        {/* <i class="fab fa-github"></i> */}
+                        
                             <p class="text-3xl font-semibold text-center text-white">TW</p>
                         </div>
                         <div class="w-2/3 flex items-center justify-center ">
@@ -67,7 +78,7 @@ class Skills extends Component {
                 <div class="shadow-lg flex flex-col justify-center bg-white border border-gray-300 rounded">
                     <div class="flex w-full h-full">
                         <div class="w-1/3 flex items-center justify-center bg-black">
-                        {/* <i class="fab fa-github"></i> */}
+                        
                             <p class="text-3xl font-semibold text-center text-white">GH</p>
                         </div>
                         <div class="w-2/3 flex items-center justify-center ">
@@ -77,29 +88,28 @@ class Skills extends Component {
                 </div>
                 <div class="shadow-lg flex flex-col justify-center bg-white border border-gray-300 rounded">
                     <div class="flex w-full h-full">
-                        <div class="w-1/3 flex items-center justify-center bg-black">
-                        {/* <i class="fab fa-github"></i> */}
-                            <p class="text-3xl font-semibold text-center text-white">GH</p>
+                        <div class="w-1/3 flex items-center justify-center bg-white">
+                    
+                            <p class="text-3xl font-semibold text-center text-green-500">MDB</p>
                         </div>
                         <div class="w-2/3 flex items-center justify-center ">
-                            <p class="text-2xl text-center text-gray-900">GitHub/Git</p>
+                            <p class="text-2xl text-center text-gray-900">MongoDB</p>
                         </div>
                     </div>
                 </div>
                 <div class="shadow-lg flex flex-col justify-center bg-white border border-gray-300 rounded">
                     <div class="flex w-full h-full">
-                        <div class="w-1/3 flex items-center justify-center bg-black">
-                        {/* <i class="fab fa-github"></i> */}
-                            <p class="text-3xl font-semibold text-center text-white">GH</p>
+                        <div class="w-1/3 flex items-center justify-center bg-gray-700">
+                            <p class="text-3xl font-semibold text-center text-green-400">N</p>
                         </div>
                         <div class="w-2/3 flex items-center justify-center ">
-                            <p class="text-2xl text-center text-gray-900">GitHub/Git</p>
+                            <p class="text-2xl text-center text-gray-900">Node.js</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-</div>
+// </div>
     );
   }
 }
--- client/src/Pages/Work.jsx
+++ client/src/Pages/Work.jsx
@@ -1,13 +1,114 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Work extends Component {
-    render() {
-        return (
-            <div>
-                      <h1>Work page</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+        <div className=''>
+
+      <div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto p-8">
+        <img
+          class="w-full"
+          src="https://i.imgur.com/YtL9W3M.png"
+          alt="Read&&Butter website page"
+        />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 text-center">Read&&Butter</div>
+          <p class="text-gray-700 text-base">
+            An open medium for anyone to post. No likes no comments no obsession. Just post and enjoy.
+          </p>
+        </div>
+        <div class="px-6 py-4 flex justify-center">
+          <button>
+            <span class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              GitHub
+            </span>
+          </button>
+          <button>
+            <a target='_blank' href='http://read-and-butter.surge.sh/' class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              Website
+            </a>
+          </button>
+        </div>
+      </div>
+
+      <div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto p-8">
+        <img
+          class="w-full"
+          src="https://i.imgur.com/FdVSz05.png"
+          alt="Read&&Butter website page"
+        />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2 text-center">Nikita Savelev</div>
+          <p class="text-gray-700 text-base">
+            The website you're currently on.
+          </p>
+        </div>
+        <div class="px-6 py-4 flex justify-center">
+          <button>
+            <span class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              GitHub
+            </span>
+          </button>
+          <button>
+            <a target='_blank' href='' class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              Website
+            </a>
+          </button>
+        </div>
+      </div>
+      <div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto p-8">
+        <img
+          class="w-full"
+          src="https://i.imgur.com/YzuxXYt.png"
+          alt="Todays weather apps"
+        />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">Todays Weather</div>
+          <p class="text-gray-700 text-base">
+           Lets check the weather.
+          </p>
+        </div>
+        <div class="px-6 py-4 flex justify-center">
+          <button>
+            <span class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              GitHub
+            </span>
+          </button>
+          <button>
+            <a target='_blank' href='https://nsavelevjs.github.io/Todays-Weather/' class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              Website
+            </a>
+          </button>
+        </div>
+      </div>
+      <div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto p-8">
+        <img
+          class="w-full"
+          src="/img/card-top.jpg"
+          alt=""
+        />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">Read&&Butter</div>
+          <p class="text-gray-700 text-base">
+            An open medium for anyone to post. No likes no comments no obsession. Just post and enjoy.
+          </p>
+        </div>
+        <div class="px-6 py-4 flex justify-center">
+          <button>
+            <a src='' class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              GitHub
+            </a>
+          </button>
+          <button>
+            <a target='_blank' href='http://read-and-butter.surge.sh/' class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              Website
+            </a>
+          </button>
+        </div>
+      </div>
+      </div>
+    );
+  }
 }
 
-export default Work;              
+export default Work;
