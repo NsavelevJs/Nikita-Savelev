78cf9575d2fec54bf296ec116b79bb2d4fccb2d5
NsavelevJS <NsavelevJS@gmail.com>
2020-07-26 23:24:25 -0400

    adding to skills section

--- client/src/Pages/Skills.jsx
+++ client/src/Pages/Skills.jsx
@@ -3,9 +3,103 @@ import React, { Component } from "react";
 class Skills extends Component {
   render() {
     return (
-      <div>
-        <h1> Hi </h1>
-      </div>
+      <div class="container mx-auto p-8 ">
+<div class="px-4 py-4 mx-auto">
+            <div class="grid sm:h-24 sm:grid-flow-row sm:gap-4 sm:grid-cols-3 grid-cols-1 md:grid-cols-4">
+                <div class="shadow-lg flex flex-col justify-center bg-white border border-gray-300 rounded">
+                    <div class="flex w-full h-full">
+                        <div class="w-1/3 flex items-center justify-center bg-yellow-500">
+                            <p class="text-3xl font-semibold text-center text-black">JS</p>
+                        </div>
+                        <div class="w-2/3 flex items-center justify-center">
+                   
+                            <p class="text-2xl text-center text-gray-900">JavaScript</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="shadow-lg flex flex-col justify-center bg-white border border-gray-300 rounded">
+                    <div class="flex w-full h-full">
+                        <div class="w-1/3 flex items-center justify-center bg-red-500">
+                        {/* <i class="far fa-gem"></i> */}
+                            <p class="text-3xl font-semibold text-center text-black">rb</p>
+                        </div>
+                        <div class="w-2/3 flex items-center justify-center ">
+                            <p class="text-2xl text-center text-gray-900">Ruby</p>
+                        </div>
+                    </div>
+                </div>
+ <div class="shadow-lg flex flex-col justify-center bg-white border border-gray-300 rounded">
+                    <div class="flex w-full h-full">
+                        <div class="w-1/3 flex items-center justify-center bg-red-500">
+                        {/* <i class="fab fa-github"></i> */}
+                            <p class="text-3xl font-semibold text-center text-black">rbr</p>
+                        </div>
+                        <div class="w-2/3 flex items-center justify-center ">
+                            <p class="text-2xl text-center text-gray-900">Ruby on Rails</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="shadow-lg flex flex-col justify-center bg-white border border-gray-300 rounded">
+                    <div class="flex w-full h-full">
+                        <div class="w-1/3 flex items-center justify-center bg-blue-500">
+                        {/* <i class="fab fa-react"></i> */}
+                            <p class="text-3xl font-semibold text-center text-black">R</p>
+                        </div>
+                        <div class="w-2/3 flex items-center justify-center ">
+                            <p class="text-2xl text-center text-gray-900">React</p>
+                        </div>
+                    </div>
+                </div>
+
+               
+                <div class="shadow-lg flex flex-col justify-center bg-white border border-gray-300 rounded">
+                    <div class="flex w-full h-full">
+                        <div class="w-1/3 flex items-center justify-center bg-teal-500">
+                        {/* <i class="fab fa-github"></i> */}
+                            <p class="text-3xl font-semibold text-center text-white">TW</p>
+                        </div>
+                        <div class="w-2/3 flex items-center justify-center ">
+                            <p class="text-2xl text-center text-gray-900">Tailwind CSS</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="shadow-lg flex flex-col justify-center bg-white border border-gray-300 rounded">
+                    <div class="flex w-full h-full">
+                        <div class="w-1/3 flex items-center justify-center bg-black">
+                        {/* <i class="fab fa-github"></i> */}
+                            <p class="text-3xl font-semibold text-center text-white">GH</p>
+                        </div>
+                        <div class="w-2/3 flex items-center justify-center ">
+                            <p class="text-2xl text-center text-gray-900">GitHub/Git</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="shadow-lg flex flex-col justify-center bg-white border border-gray-300 rounded">
+                    <div class="flex w-full h-full">
+                        <div class="w-1/3 flex items-center justify-center bg-black">
+                        {/* <i class="fab fa-github"></i> */}
+                            <p class="text-3xl font-semibold text-center text-white">GH</p>
+                        </div>
+                        <div class="w-2/3 flex items-center justify-center ">
+                            <p class="text-2xl text-center text-gray-900">GitHub/Git</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="shadow-lg flex flex-col justify-center bg-white border border-gray-300 rounded">
+                    <div class="flex w-full h-full">
+                        <div class="w-1/3 flex items-center justify-center bg-black">
+                        {/* <i class="fab fa-github"></i> */}
+                            <p class="text-3xl font-semibold text-center text-white">GH</p>
+                        </div>
+                        <div class="w-2/3 flex items-center justify-center ">
+                            <p class="text-2xl text-center text-gray-900">GitHub/Git</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
     );
   }
 }
