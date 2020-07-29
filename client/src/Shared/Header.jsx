d87fa73f4a50bafa8035aab4f1a5130b9e4c626a
NsavelevJS <NsavelevJS@gmail.com>
2020-07-28 22:40:55 -0400

    Made class, className in header

--- client/src/Shared/Header.jsx
+++ client/src/Shared/Header.jsx
@@ -4,34 +4,34 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <ul class="flex border-b ">
-          <li class="-mb-px mr-5">
+        <ul className="flex border-b ">
+          <li className="-mb-px mr-5">
             <a
-              class="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-orange-500 font-semibold"
+              className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-orange-500 font-semibold"
               href="/"
             >
               Nikita Savelev
             </a>
           </li>
-          <li class="mr-1">
+          <li className="mr-1">
             <a
-              class="bg-white inline-block py-2 px-4 text-orange-500 hover:text-orange-800 font-semibold"
+              className="bg-white inline-block py-2 px-4 text-orange-500 hover:text-orange-800 font-semibold"
               href="/skills"
             >
               Skills
             </a>
           </li>
-          <li class="mr-1">
+          <li className="mr-1">
             <a
-              class="bg-white inline-block py-2 px-4 text-orange-500 hover:text-orange-800 font-semibold"
+              className="bg-white inline-block py-2 px-4 text-orange-500 hover:text-orange-800 font-semibold"
               href="/work"
             >
               Work
             </a>
           </li>
-          <li class="mr-1">
+          <li className="mr-1">
             <a
-              class="bg-white inline-block py-2 px-4 text-orange-500 hover:text-orange-800 font-semibold"
+              className="bg-white inline-block py-2 px-4 text-orange-500 hover:text-orange-800 font-semibold"
               href="/contact"
             >
               Contact Me
